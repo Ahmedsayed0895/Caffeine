1f70158ff0a8ef608535e1e293c3a3aa0db1e122
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-07-01 16:21:54 +0300

    feat: set app icon

---
+++ app/src/main/java/com/example/caffeine/screen/StartScreen.kt
@@ -0,0 +1,8 @@
+package com.example.caffeine.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StartScreen(){
+
+}
--- app/src/main/java/com/example/caffeine/ui/theme/Theme.kt
+++ app/src/main/java/com/example/caffeine/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = primaryWhite,
+    secondary = beigeWhite,
+    tertiary = brown
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = brown,
+    secondary = darkBrown,
+    tertiary = darkestBrown
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
