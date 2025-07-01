164126c35bea8490f2a228d5896e7fd065240728
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-07-01 16:28:01 +0300

    feat: Add StartScreen composable and configure VCS and Gradle settings

---
+++ app/src/main/java/com/example/caffeine/screen/StartScreen.kt
@@ -0,0 +1,19 @@
+package com.example.caffeine.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StartScreen(){
+    Scaffold(){ contentPadding ->
+        Column(
+            modifier = Modifier.padding(contentPadding)
+        ){
+
+        }
+
+    }
+}
