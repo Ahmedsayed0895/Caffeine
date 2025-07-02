80eb3109c301cd229b8988421f84d65c30c65771
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-07-02 05:46:18 +0300

    add: Add header with avatar and add button to StartScreen

--- app/src/main/java/com/example/caffeine/screen/StartScreen.kt
+++ app/src/main/java/com/example/caffeine/screen/StartScreen.kt
@@ -1,10 +1,30 @@
 package com.example.caffeine.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.caffeine.ui.theme.darkBrown
+import com.example.caffeine.R
+import com.example.caffeine.ui.theme.darkestBrown
+import com.example.caffeine.ui.theme.primaryWhite
 
 @Composable
 fun StartScreen(){
@@ -12,6 +32,44 @@ fun StartScreen(){
         Column(
             modifier = Modifier.padding(contentPadding)
         ){
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(16.dp)
+            )
+            {
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(shape = CircleShape)
+                        .clickable{}
+
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.coffe_avatar),
+                        contentDescription = "Avatar",
+                    )
+                }
+                Spacer(Modifier.weight(1f))
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(shape = CircleShape)
+                        .clickable{}
+                        .background(primaryWhite),
+                    contentAlignment = Alignment.Center
+                ){
+                    Icon(
+                        painter = painterResource(R.drawable.add),
+                        contentDescription = "add",
+                        tint = darkestBrown,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+
+
+            }
 
         }
 
