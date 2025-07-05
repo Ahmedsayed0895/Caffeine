ca33157ea167f57a060c3edd40e06ef75bdcec0c
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-07-05 11:44:30 +0300

    refactor: Create OrderReadyMessage composable

--- app/src/main/java/com/example/caffeine/MainActivity.kt
+++ app/src/main/java/com/example/caffeine/MainActivity.kt
@@ -4,12 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.example.caffeine.screen.OrderDoneScreen
 import com.example.caffeine.ui.theme.CaffeineTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,13 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CaffeineTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    OrderDoneScreen()
-                }
+
             }
         }
     }
--- app/src/main/java/com/example/caffeine/screen/OrderDoneScreen.kt
+++ app/src/main/java/com/example/caffeine/screen/OrderDoneScreen.kt
@@ -1,37 +1,23 @@
 package com.example.caffeine.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.caffeine.R
 import com.example.caffeine.component.CoffeeSeparatedCup
 import com.example.caffeine.component.CoffeeSwitchButton
 import com.example.caffeine.component.ExitHeader
 import com.example.caffeine.component.IconTextButton
-import com.example.caffeine.extension.dropShadow
-import com.example.caffeine.ui.theme.Urbanist
+import com.example.caffeine.component.OrderReadyMessage
 
 @Composable
 fun OrderDoneScreen() {
@@ -48,43 +34,7 @@ fun OrderDoneScreen() {
 
             ) {
             ExitHeader(bottomSpace = 16.dp, modifier = Modifier.padding(horizontal = 16.dp))
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = CenterHorizontally,
-                modifier = Modifier.width(270.dp)
-            ) {
-
-                Box(
-                    Modifier
-                        .padding(bottom = 24.dp)
-                        .dropShadow(
-                            shape = CircleShape,
-                            color = Color(0x80B94B23),
-                            blur = 16.dp,
-                            offsetY = 6.dp,
-                        )
-                        .size(56.dp)
-                        .clip(CircleShape)
-                        .background(Color(0xFF7C351B))
-                        .padding(horizontal = 10.dp, vertical = 8.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.tick),
-                        contentDescription = "Product Image",
-                        tint = Color(0xDEFFFFFF),
-                    )
-                }
-                Text(
-                    text = "Your coffee is ready,\nEnjoy",
-                    color = Color(0xDE1F1F1F),
-                    fontSize = 22.sp,
-                    fontFamily = Urbanist,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    letterSpacing = 0.25.sp,
-                )
-            }
+            OrderReadyMessage()
             CoffeeSeparatedCup()
             Spacer(modifier = Modifier.weight(1f))
             CoffeeSwitchButton()
@@ -94,11 +44,11 @@ fun OrderDoneScreen() {
                 text = "Take snack",
                 icon = painterResource(R.drawable.arrow_right)
             )
-
         }
 
     }
 }
+
 
 
 
